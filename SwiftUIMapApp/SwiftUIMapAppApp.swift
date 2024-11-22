102d479f0e40bc5fa1e458e5c78c34a022850d5f
adkhamjon <adkhamjon.rakhimov.dev@gmail.com>
2024-11-22 17:20:33 +0500

    Initial Commit

---
+++ SwiftUIMapApp/SwiftUIMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapAppApp.swift
+//  SwiftUIMapApp
+//
+//  Created by Adkhamjon Rakhimov on 22/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
